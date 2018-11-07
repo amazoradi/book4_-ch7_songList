9e8aae344e7a57e47fb0cc44f728693f0d89c548
amazoradi <amazoradi@gmail.com>
2018-11-07 09:18:14 -0600

    practice complete, song list appears on DOM on the click of the button

--- songs.js
+++ songs.js
@@ -3,58 +3,31 @@ $(document).ready(function () {
   let loadSong = $("#load-songs")
   // Use jQuery to get a reference to `song-list`
   let listSong = $("#song-list")
-
-  function loadToDoc() {
   /*
       Attach a click handler to the button with jQuery. When
       the button is clicked, use $.ajax() to load `songs.json`
       from the file system
   */
-    $(loadSong).click(() => {
-      console.log("click")
-      $.ajax({
-        type: "GET",
-        url: "http://localhost:8088/songs",
-        success: function (song) {
-          console.log("success", song)
-        }
-
-        // Instructions for how to deserialize a `mycustomtype`
-        //  converters: {
-        // 'text mycustomtype': function (result) {
-        // Do Stuff
-        // return newresult;
-        // }
-        // },
-      }).then(songs => console.log("then", songs))
+  $(loadSong).click(() => {
+    $.ajax({
+      type: "GET",
+      url: "http://localhost:8088/songs",
     })
-  }
-  loadToDoc()
-
-
+      /*
+          Chain a `.then()` method to the ajax call, and when
+          it is complete build a DOM component for each song with
+          the following structure. Use the jQuery append() method
+          to put an HTML representation of each song the DOM as a
+          child component of the .
+      */
+    .then(songs => $.each(songs, function (i, song) {
+      listSong.append(`<section class="song"><h2 class="song__title"> ${song.title}</h1>
+          <section class="song__description">
+            Performed by ${song.artist} on the album ${song.album}
+          </section></section>`)
+    })
+    )
+  })
 })
 
-  
 
-  
-
-    //     // Expect a `mycustomtype` back from server
-    //     dataType: 'mycustomtype'
-    //   });
-
-
-    /*
-        Chain a `.then()` method to the ajax call, and when
-        it is complete build a DOM component for each song with
-        the following structure. Use the jQuery append() method
-        to put an HTML representation of each song the DOM as a
-        child component of the .
-  
-            <section class="song">
-                <h1 class="song__title">{Title of song}</h1>
-                <section class="song__description">
-                    Performed by {artist} on the album {album}
-                </section>
-            </section>
-    */
-  // })
